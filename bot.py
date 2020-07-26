db8872dd0a811b9d789fad2102863c075f30e443
pkuzmichev <petya.kuzmichev@gmail.com>
2020-07-26 18:47:14 +0300

    inline telegram bot

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 import telebot
-
+from telebot import types
 from quick_response_code import QuickResponseCode
 
 bot = telebot.TeleBot(os.environ.get('BOT_API'), parse_mode=None)
@@ -19,6 +19,20 @@ def send_qr(message):
     with open('qr_code/qr_code.png', 'rb') as f:
         contents = f.read()
         bot.send_photo(chat_id=message.chat.id, photo=contents, caption=message.text)
+
+
+@bot.inline_handler(func=lambda query: len(query.query) > 0)
+def query_text(query):
+    hint = 'Input text or link and I generate QR code for you!'
+    r = types.InlineQueryResultArticle(
+        id='1',
+        title='QR Bot',
+        description=hint,
+        input_message_content=types.InputTextMessageContent(
+            message_text='No data :('
+        )
+    )
+    bot.answer_inline_query(query.id, [r])
 
 
 if __name__ == '__main__':
