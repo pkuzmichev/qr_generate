466192c4ecdf1db988d49ab40d80ab22ad683bf3
pkuzmichev <petya.kuzmichev@gmail.com>
2020-08-09 19:37:57 +0300

    test inline bot

--- bot.py
+++ bot.py
@@ -34,8 +34,12 @@ def inline_cached_photo(query):
 
     qr.generate_qr_code(query.query)
 
+
+    # 1316606
     if query:
-        info_photo = bot.send_photo(chat_id='1316606', photo=open('qr_code/qr_code.png', 'rb'), caption=query.query)
+        info_photo = bot.send_photo(chat_id=query.from_user.id,
+                                    photo=open('qr_code/qr_code.png', 'rb'),
+                                    caption=query.query)
         print('original photo', info_photo['photo'][-1]['file_id'])
         thumb_photo = info_photo['photo'][0]['file_id']
         original_photo = info_photo['photo'][-1]['file_id']
